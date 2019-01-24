68506ccb6e0b8bd7cc4923667b60587da4b7f201
Ramesh R <ramesh.myoffice@gmail.com>
2019-01-24 07:16:55 +0530

    adding support for user defined URL inputs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# skiing-challenge
+# Introduction
+Utility is written in node.js.
+
+# Install nodejs
+https://nodejs.org/
+
+# To start utility(from CLI)
+`npm start`
+
+# User defined Input
+`node ./test  --DATA_URL={HTTP URL}`
--- test/index.js
+++ test/index.js
@@ -1,10 +1,20 @@
 const Skiing = require('../');
 const path = require('path');
 
+var argv = require('minimist')(process.argv.slice(2));
+
 const skiing = new Skiing();
 
-const DATA_URL = 'http://s3-ap-southeast-1.amazonaws.com/geeks.redmart.com/coding-problems/map.txt';
 const FILE_PATH = path.resolve(__dirname, './map.txt');
+
+let DATA_URL = 'http://s3-ap-southeast-1.amazonaws.com/geeks.redmart.com/coding-problems/map.txt';
+
+if (argv.DATA_URL) {
+    console.log('Reading data from user defined data URL. :)');
+    console.log('URL: ', argv.DATA_URL);
+
+    DATA_URL = argv.DATA_URL;
+}
 
 (async function findPath() {
 
@@ -15,4 +25,9 @@ const FILE_PATH = path.resolve(__dirname, './map.txt');
     console.timeEnd('SKI');
 
     console.log('Path Length: ', deepPath.length, ', Path: ', deepPath);
+
+    const dropSize = deepPath.length;
+    const largestDrop = deepPath[0] - deepPath[deepPath.length - 1];
+
+    console.log('Size of drop ', dropSize, ', Largest drop: ', largestDrop);
 })();
